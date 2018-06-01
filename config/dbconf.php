4e9335ab7ad0f2b3706ce7ec4185f0910a85608f
jiaqi.shen@capgemini.com <jiaqi.shen@capgemini.com>
2018-06-01 22:32:31 +0800

    origin wow web code

---
+++ config/dbconf.php
@@ -0,0 +1,9 @@
+<?php
+$dbhost = "47.93.198.61";
+$dbuser = "root";
+$dbpass = "root";
+$accdb = "auth";
+$chardb = "characters";
+
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $accdb);
+?>
---
+++ config/webconf.php
@@ -0,0 +1,4 @@
+<?php
+$servertitle = "骨头魔兽"; //Server title will be displayed on the tab and on the navbar
+$realmlist = "47.93.198.61"; //realmlist. Do NOT include "set realmlist"
+?>
